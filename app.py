886851e93d89b546b55922026c835f81bb597b58
purna <purna@gmail.com>
2024-01-12 18:39:46 +0530

    test

---
+++ app.py
@@ -0,0 +1,18 @@
+# write calculator python code
+def add(x, y):
+    """This function adds two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """This function subtracts two numbers"""
+    return x - y
+
+def multiply(x, y):
+    """This function multiplies two numbers"""
+    return x * y
+
+def divide(x, y):
+    """This function divides two numbers"""
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
